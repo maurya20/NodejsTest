6157503e44ab5f1e4ad881173f6e9adc0219a77b
maurya20 <mukesh.ice17@gmail.com>
2020-10-05 17:01:22 +0530

    append file implemented.

--- test.js
+++ test.js
@@ -1,17 +1,10 @@
-var http = require('http');
-var url = require('url');
-var fs = require('fs');
-
-http.createServer(function (req, res) {
-  var q = url.parse(req.url, true);
-  var filename = "." + q.pathname;
-  fs.readFile(filename, function(err, data) {
-    if (err) {
-      res.writeHead(404, {'Content-Type': 'text/html'});
-      return res.end("404 Not Found");
-    } 
-    res.writeHead(200, {'Content-Type': 'text/html'});
-    res.write(data);
-    return res.end();
-  });
-}).listen(8080);
+const fs = require('fs')
+const os = require('os')
+const user = os.userInfo()
+const date = new Date()
+let message = `user '${user.username}' started app at ${date}   hiiiiiii`
+fs.appendFile("hello.txt", message,(err)=>{
+    if(err){
+        console.log("Something went wrong while appending")
+    }
+})
