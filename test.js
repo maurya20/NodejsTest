cf76c412730902d8453699657fcf1ed2f61c2936
maurya20 <mukesh.ice17@gmail.com>
2020-10-07 09:42:10 +0530

    Node server created from scratch

--- test.js
+++ test.js
@@ -1,10 +1,6 @@
-const fs = require('fs')
-const os = require('os')
-const user = os.userInfo()
-const date = new Date()
-let message = `user '${user.username}' started app at ${date}   hiiiiiii`
-fs.appendFile("hello.txt", message,(err)=>{
-    if(err){
-        console.log("Something went wrong while appending")
-    }
-})
+const http = require('http')
+http.createServer((request,response)=>{
+    response.writeHead(200,{'Content-type':'text/plain'})
+    response.end("Hello there, This is my first node server")
+}).listen(8181)
+console.log("Server is running at port: 8181")
