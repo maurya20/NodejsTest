faf38b859bc223d3f46e0635a675a04c1b36e33d
maurya20 <mukesh.ice17@gmail.com>
2020-10-19 16:02:20 +0530

    jwt implemented on root level

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
 const bcrypt = require('bcrypt')
 const {MD5} = require('crypto-js')
+const jwt = require('jsonwebtoken')
 
+// const mySalt = "qwerty"
+// const user ={
+//     id:1,
+//     token: MD5('asdf').toString()+mySalt
+// }
 
-const user ={
-    id:1,
-    token: MD5('asdf').toString()
-}
-
-console.log(user)
+// console.log(user)
+const id = '1000'
+const secret = "supersecret"
+const token = jwt.sign(id,secret)
+console.log(token)
+const receivedToken = "eyJhbGciOiJIUzI1NiJ9.MTAwMA.L9PmEqLlZjettygguzj25agunJu6NkvVtG9RFRBnK2Y"
+const decodeToken = jwt.verify(receivedToken,secret)
+console.log(decodeToken)
