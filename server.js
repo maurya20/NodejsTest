8d2fcfec2594d0c80d0d3f628a6fbed2a2aea511
maurya20 <mukesh.ice17@gmail.com>
2020-10-19 11:25:39 +0530

    find method implemented in mongoose

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 mongoose.Promise = global.Promise
-mongoose.connect('mongodb://localhost:27017/test')
+mongoose.connect('mongodb://localhost:27017/test',{useNewUrlParser: true}, {useUnifiedTopology: true })
 
 const carSchema = mongoose.Schema({
     brand:String,
@@ -11,14 +11,19 @@ const carSchema = mongoose.Schema({
 })
 
 const Car = mongoose.model("Car", carSchema)
-const adCar = new Car({
-    brand:"Ford",
-    model:"Fiesta",
-    year:2008,
-    available:false
-})
+// const adCar = new Car({
+//     brand:"Ford",
+//     model:"Mustang",
+//     year:2006,
+//     available:false
+// })
 
-adCar.save((err,doc)=>{
+// adCar.save((err,doc)=>{
+//     if(err) return console.log(err)
+//     console.log(doc)
+// })
+
+Car.find((err,doc)=>{
     if(err) return console.log(err)
     console.log(doc)
 })
